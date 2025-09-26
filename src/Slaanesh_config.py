67cf1c96c0e9ef7cb1b5caab9beb5187cb1a5106
Jahn16 <jpdeoliveiraferreira@gmail.com>
2025-09-26 10:24:43 -0300

    feat: add pydantic models

--- src/Slaanesh_config.py
+++ src/Slaanesh_config.py
@@ -1,91 +1,119 @@
 import configparser
+from pydantic import BaseModel, Field
 
 version = "0.70-beta"
 
 # local files
-game_list = r'../files/database/gamelist.feather'
-playthrough_list = r'../files/database/playthroughs.feather'
-file_icon = r'../files/assets/Slaanesh.png'
-file_config = r'../files/config/config.ini'
-path_covers = r'../files/covers/'
-path_import = r'../files/import/'
-path_export = r'../files/export/'
+game_list = r"../files/database/gamelist.feather"
+playthrough_list = r"../files/database/playthroughs.feather"
+file_icon = r"../files/assets/Slaanesh.png"
+file_config = r"../files/config/config.ini"
+path_covers = r"../files/covers/"
+path_import = r"../files/import/"
+path_export = r"../files/export/"
 # serving files,
-server_file_icon = r'/assets/Slaanesh.png'
-server_path_covers = r'/covers'
-server_path_export = r'/export'
-#display types
-display_types = ['cards', 'table']
+server_file_icon = r"/assets/Slaanesh.png"
+server_path_covers = r"/covers"
+server_path_export = r"/export"
+# display types
+display_types = ["cards", "table"]
 
 config_dictionary = {
-    #values in config.ini
-    'ui': {
-        'name': 'Slaanesh',
-        'native': False,
-        'color_coding': True,
-        'dark_mode': True,
-        'row_height': 96,
-        'card_width': 310,
+    # values in config.ini
+    "ui": {
+        "name": "Slaanesh",
+        "native": False,
+        "color_coding": True,
+        "dark_mode": True,
+        "row_height": 96,
+        "card_width": 310,
     },
-    'tabs': {
-        'type_playing': 'cards',
-        'type_played': 'table',
-        'type_backlog': 'cards',
-        'type_wishlist': 'table',
-        'filter_playing': True,
-        'filter_played': True,
-        'filter_backlog': False,
-        'filter_wishlist': False,
+    "tabs": {
+        "type_playing": "cards",
+        "type_played": "table",
+        "type_backlog": "cards",
+        "type_wishlist": "table",
+        "filter_playing": True,
+        "filter_played": True,
+        "filter_backlog": False,
+        "filter_wishlist": False,
     },
-    'igdb': {
-        'client_id': '',
-        'client_secret': '',
-        'auth_token': '',
-        'token_timestamp': '',
-        'data_refresh_limit': 1
+    "igdb": {
+        "client_id": "",
+        "client_secret": "",
+        "auth_token": "",
+        "token_timestamp": "",
+        "data_refresh_limit": 1,
     },
-    'platforms': [
-        'PC', 
-        'PlayStation', 
-        'Xbox',
-        'Nintendo', 
-        'VR'
-    ],
-    'playing': [
-        'playing', 
-        'on hold'
-    ],
-    'played positive': [
-        'completed', 
-        'mastered'
-    ],
-    'played negative': [
-        'discarded'
-    ],
-    'played': [],
-    'backlog': [
-        'backlog', 
-        'waiting'
-    ],
-    'wishlist': [
-        'wishlist'
-    ],
-    'unplayed': [],
+    "platforms": ["PC", "PlayStation", "Xbox", "Nintendo", "VR"],
+    "playing": ["playing", "on hold"],
+    "played positive": ["completed", "mastered"],
+    "played negative": ["discarded"],
+    "played": [],
+    "backlog": ["backlog", "waiting"],
+    "wishlist": ["wishlist"],
+    "unplayed": [],
 }
+
+
+class UIConfig(BaseModel):
+    name: str = "Slaanesh"
+    native: bool = False
+    color_coding: bool = True
+    dark_mode: bool = True
+    row_height: int = 96
+    card_width: int = 310
+
+
+class TabsConfig(BaseModel):
+    type_playing: str = "cards"
+    type_played: str = "table"
+    type_backlog: str = "cards"
+    type_wishlist: str = "table"
+    filter_playing: bool = True
+    filter_played: bool = True
+    filter_backlog: bool = False
+    filter_wishlist: bool = False
+
+
+class IGDBConfig(BaseModel):
+    client_id: str = Field(...)
+    client_secret: str = Field(...)
+    auth_token: str = ""
+    token_timestamp: str = "2000-01-01 12:12:12"
+    data_refresh_limit: int = 1
+
+
+class Settings(BaseModel):
+    ui: UIConfig = UIConfig()
+    tabs: TabsConfig = TabsConfig()
+    igdb: IGDBConfig = IGDBConfig()
+    platforms: list[str] = ["PC", "PlayStation", "Xbox", "Nintendo", "VR"]
+    playing: list[str] = ["playing", "on hold"]
+    played_positive: list[str] = Field(
+        default=["completed", "mastered"], alias="played positive"
+    )
+    played_negative: list[str] = Field(default=["discarded"], alias="played negative")
+    played: list[str] = []
+    backlog: list[str] = ["backlog", "waiting"]
+    wishlist: list[str] = ["wishlist"]
+    unplayed: list[str] = []
+
 
 config = configparser.ConfigParser(allow_no_value=True)
 config.optionxform = str
+
 
 def load_config():
     global config_dictionary
     config.read(file_config)
 
     for section in config:
-        #skip unused sections
+        # skip unused sections
         if section not in config_dictionary:
             continue
 
-        #list
+        # list
         if type(config_dictionary[section]) is list:
             config_dictionary[section].clear()
             for key in config[section]:
@@ -95,16 +123,26 @@ def load_config():
         for key in config[section]:
             # int
             if config[section][key].isnumeric():
-                config_dictionary[section][key] = config.getint(section, key, fallback=config_dictionary[section][key])
+                config_dictionary[section][key] = config.getint(
+                    section, key, fallback=config_dictionary[section][key]
+                )
             # boolean
-            elif config[section][key] == 'True' or config[section][key] == 'False':
-                config_dictionary[section][key] = config.getboolean(section, key, fallback=config_dictionary[section][key])
+            elif config[section][key] == "True" or config[section][key] == "False":
+                config_dictionary[section][key] = config.getboolean(
+                    section, key, fallback=config_dictionary[section][key]
+                )
             # other, treat as string
             else:
                 config_dictionary[section][key] = config[section][key]
 
-    config_dictionary['played'] = config_dictionary['played positive'] + config_dictionary['played negative']
-    config_dictionary['unplayed'] = config_dictionary['backlog'] + config_dictionary['wishlist'] + config_dictionary['playing']
+    config_dictionary["played"] = (
+        config_dictionary["played positive"] + config_dictionary["played negative"]
+    )
+    config_dictionary["unplayed"] = (
+        config_dictionary["backlog"]
+        + config_dictionary["wishlist"]
+        + config_dictionary["playing"]
+    )
 
 
 def update_config(updates):
@@ -113,10 +151,10 @@ def update_config(updates):
         if update[0] is not None:
             if update[0] not in config:
                 config.add_section(update[0])
-            
+
             # lists
             if update[1] is None and update[2] is not None:
-                clist = update[2].split(sep=',')
+                clist = update[2].split(sep=",")
                 config_dictionary[update[0]] = clist
                 config[update[0]].clear()
                 for item in clist:
@@ -133,7 +171,9 @@ def update_config(updates):
                     config_dictionary[update[0]][update[1]] = int(update[2])
                     config[update[0]][update[1]] = str(int(update[2]))
                 else:
-                    config[update[0]][update[1]] = config_dictionary[update[0]][update[1]] = str(update[2])
+                    config[update[0]][update[1]] = config_dictionary[update[0]][
+                        update[1]
+                    ] = str(update[2])
 
-    with open(file_config, 'w') as configfile: # noinspection PyTypeChecker
+    with open(file_config, "w") as configfile:  # noinspection PyTypeChecker
         config.write(configfile)
